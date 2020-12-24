42324456af04d6114a991b3956ef94c9adafdf25
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2020-12-24 12:01:05 +0330

    Plots the real and predicted chart

---
+++ Ploting.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+
+def plot(unscaled_y_test, y_test_predicted):
+    plt.gcf().set_size_inches(22, 15, forward=True)
+
+    start = 0
+    end = -1
+
+    real = plt.plot(unscaled_y_test[start:end], label='real')
+    pred = plt.plot(y_test_predicted[start:end], label='predicted')
+
+    plt.legend(['Real', 'Predicted'])
+
+    return plt.show()
