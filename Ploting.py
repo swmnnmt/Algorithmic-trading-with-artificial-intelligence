6b54397c35986dde36ca1b80a01a8596d85b9259
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2020-12-25 10:14:58 +0330

    Ploting module for visualize

--- Ploting.py
+++ Ploting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def plot(unscaled_y_test, y_test_predicted):
     plt.gcf().set_size_inches(22, 15, forward=True)
 
-    start = 0
+    start = 160
     end = -1
 
     real = plt.plot(unscaled_y_test[start:end], label='real')
