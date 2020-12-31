6296ac3e1ffab528691ebc7796294dbde41c5378
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2020-12-31 12:14:49 +0330

    3rd version : trading algorithm now avaliable!

--- BakeOff.py
+++ BakeOff.py
@@ -1,12 +1,14 @@
 from LSTM import lstm_model
 from Ploting import plot
 from Preprocessing import preprocess
+from Trading_Algorithm import trading_algorithm
 
-csv_path = 'data/femeli-daily.csv'
+csv_path = 'data/tickers_data/فولاد.csv'
 history_points = 50
 
 x_train, y_train, x_test, y_test, x_val, y_val, y_test_real, scale_back, tech_ind_train, tech_ind_test, tech_ind_val = preprocess(
-        csv_path, history_points)
-y_test_predicted = lstm_model(history_points, x_train, y_train, x_test, y_test, x_val, y_val, y_test_real,
-                                  scale_back, tech_ind_train, tech_ind_test, tech_ind_val)
-plot(y_test_real, y_test_predicted)
+    csv_path, history_points)
+y_test_predicted, model = lstm_model(history_points, x_train, y_train, x_test, y_test, x_val, y_val, y_test_real,
+                                     scale_back, tech_ind_train, tech_ind_test, tech_ind_val)
+buys, sells = trading_algorithm(x_test, tech_ind_test, scale_back, model)
+plot(y_test_real, y_test_predicted, buys, sells)
