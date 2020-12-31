70efae0626925e189b784455525480c985582a95
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2020-12-31 12:24:01 +0330

    3rd version : Plots buy and sell points on stock chart

--- Ploting.py
+++ Ploting.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def plot(y_test_real, y_test_predicted):
+def plot(y_test_real, y_test_predicted, buys, sells):
     plt.gcf().set_size_inches(22, 15, forward=True)
 
-    start = 160
+    start = 0
     end = -1
 
-    real = plt.plot(y_test_real[start:end], label='real')
-    pred = plt.plot(y_test_predicted[start:end], label='predicted')
+    plt.plot(y_test_real[start:end], label='real')
+    plt.plot(y_test_predicted[start:end], label='predicted')
+
+    plt.scatter(list(list(zip(*buys))[0]), list(list(zip(*buys))[1]), c='#00ff00')
+    plt.scatter(list(list(zip(*sells))[0]), list(list(zip(*sells))[1]), c='#ff0000')
 
     plt.legend(['Real', 'Predicted'])
 
