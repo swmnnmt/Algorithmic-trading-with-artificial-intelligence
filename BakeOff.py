fd577988c54184849b0ee75a0a7f40555fbdbbf7
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2021-01-02 03:04:55 +0330

    4th version : Computed earnings for test data available

--- BakeOff.py
+++ BakeOff.py
@@ -1,9 +1,9 @@
 from LSTM import lstm_model
 from Ploting import plot
 from Preprocessing import preprocess
-from Trading_Algorithm import trading_algorithm
+from Trading_Algorithm import trading_algorithm, compute_earnings
 
-csv_path = 'data/tickers_data/فولاد.csv'
+csv_path = 'data/tickers_data/femeli-daily.csv'
 history_points = 50
 
 x_train, y_train, x_test, y_test, x_val, y_val, y_test_real, scale_back, tech_ind_train, tech_ind_test, tech_ind_val = preprocess(
@@ -12,3 +12,8 @@ y_test_predicted, model = lstm_model(history_points, x_train, y_train, x_test, y
                                      scale_back, tech_ind_train, tech_ind_test, tech_ind_val)
 buys, sells = trading_algorithm(x_test, tech_ind_test, scale_back, model)
 plot(y_test_real, y_test_predicted, buys, sells)
+purchase_amt = 1000000000  # 100 million Toman
+print("{} Rials after trading over trading days of the stock will make profit {} Rials".format(purchase_amt,
+                                                                                               compute_earnings(buys,
+                                                                                                                sells,
+                                                                                                                purchase_amt) - purchase_amt))
