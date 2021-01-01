b78c409bc7fd875157da357045a207e26ee01504
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2021-01-02 03:03:02 +0330

    Module for compute earnings added

--- Trading_Algorithm.py
+++ Trading_Algorithm.py
@@ -19,3 +19,20 @@ def trading_algorithm(x_test, tech_ind_test, scale_back, model):
             sells.append((x, price_today[0][0]))
         x += 1
     return buys, sells
+
+
+def compute_earnings(buys, sells, purchase_amt):
+    stock = 0
+    balance = 0
+    while len(buys) > 0 and len(sells) > 0:
+        if buys[0][0] < sells[0][0]:
+            # time to buy with all of the money we have
+            balance -= purchase_amt
+            stock += purchase_amt / buys[0][1]
+            buys.pop(0)
+        else:
+            # time to sell all of our stock
+            balance += stock * sells[0][1]
+            stock = 0
+            sells.pop(0)
+    return balance
