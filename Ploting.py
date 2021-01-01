f28005df5d74770720477df85c6e51fb451e84e0
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2021-01-02 03:01:44 +0330

    Better visualization for buy/sell points

--- Ploting.py
+++ Ploting.py
@@ -10,9 +10,9 @@ def plot(y_test_real, y_test_predicted, buys, sells):
     plt.plot(y_test_real[start:end], label='real')
     plt.plot(y_test_predicted[start:end], label='predicted')
 
-    plt.scatter(list(list(zip(*buys))[0]), list(list(zip(*buys))[1]), c='#00ff00')
-    plt.scatter(list(list(zip(*sells))[0]), list(list(zip(*sells))[1]), c='#ff0000')
+    plt.scatter(list(list(zip(*buys))[0]), list(list(zip(*buys))[1]), c='#00ff00', label='Buy')
+    plt.scatter(list(list(zip(*sells))[0]), list(list(zip(*sells))[1]), c='#ff0000', label='Sell')
 
-    plt.legend(['Real', 'Predicted'])
+    plt.legend(['Real', 'Predicted', 'Buy', 'Sell'])
 
     return plt.show()
