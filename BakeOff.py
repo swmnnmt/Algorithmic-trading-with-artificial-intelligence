74ea2a8701405b2e092d272387850b350edc9703
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2021-01-15 21:56:05 +0330

    Earnings computed for test data (15% of whole data)

--- BakeOff.py
+++ BakeOff.py
@@ -13,7 +13,8 @@ y_test_predicted, model = lstm_model(history_points, x_train, y_train, x_test, y
 buys, sells = trading_algorithm(x_test, tech_ind_test, scale_back, model)
 plot(y_test_real, y_test_predicted, buys, sells)
 purchase_amt = 1000000000  # 100 million Toman
-print("{} Rials after trading over trading days of the stock will make profit {} Rials".format(purchase_amt,
-                                                                                               compute_earnings(buys,
-                                                                                                                sells,
-                                                                                                                purchase_amt) - purchase_amt))
+print("{} Rials after trading over trading days of the test data will make profit {} Rials".format(purchase_amt,
+                                                                                                   compute_earnings(
+                                                                                                       buys,
+                                                                                                       sells,
+                                                                                                       purchase_amt) - purchase_amt))
